d27227d3cc4f626c374882faf151e951191fd1c6
JesusCabral <JesusCabral@host.docker.internal>
2024-02-09 13:37:34 +0100

    primer commit

---
+++ src/test2/Test2.java
@@ -0,0 +1,16 @@
+package test2;
+
+/**
+ *
+ * @author JesusCabral
+ */
+public class Test2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
