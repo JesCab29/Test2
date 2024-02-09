7c1d287bd319a07deef2ac4dd6d3ac53d625c10b
JesusCabral <JesusCabral@host.docker.internal>
2024-02-09 13:43:48 +0100

    primer committt

--- src/test2/Test2.java
+++ src/test2/Test2.java
@@ -10,7 +10,7 @@ public class Test2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Holaaaa");
     }
     
 }
